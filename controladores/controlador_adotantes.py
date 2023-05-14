5bbb5c77206f1892909077edcea7ab7c93ea932e
Higor Abreu <higorabreuf@gmail.com>
2023-05-14 00:43:04 -0300

    alt

--- controladores/controlador_adotantes.py
+++ controladores/controlador_adotantes.py
@@ -18,6 +18,7 @@ class ControladorAdotantes:
         tipo_habitacao = self.__controlador_sistema.controlador_tipos_habitacao.incluir_tipo_habitacao()
         print(tipo_habitacao)
         dados_adotante = self.__tela_adotante.pega_dados_adotante()
+
         cpf_valido = self.pega_adotante_por_cpf(dados_adotante['cpf'])
         if cpf_valido is None:
             tem_outros_animais = dados_adotante["tem_outros_animais"].upper()
@@ -27,14 +28,23 @@ class ControladorAdotantes:
                 else:
                     tem_outros_animais = False
                 adotante = Adotante(
+<<<<<<< Updated upstream
                     dados_adotante["cpf"], dados_adotante["nome"], dados_adotante["nascimento"], dados_adotante["endereco"], tem_outros_animais, tipo_habitacao)
+=======
+                    dados_adotante["cpf"], dados_adotante["nome"], dados_adotante["nascimento"], dados_adotante["endereco"], tem_outros_animais)
+>>>>>>> Stashed changes
                 self.__adotantes.append(adotante)
                 self.__tela_adotante.mostra_mensagem(
                     "Adotante cadastrado com sucesso no sistema")
             else:
                 self.__tela_adotante.mostra_mensagem(
+<<<<<<< Updated upstream
                     "ERRO: informações inválidas, digite novamente os dados:")
                 self.__tela_adotante.pega_dados_adotante()  # nao seria necessario um while(?)
+=======
+                    "ERRO: informações inválidas, tente novamente")
+                self.__tela_adotante.pega_dados_adotante()
+>>>>>>> Stashed changes
         else:
             self.__tela_adotante.mostra_mensagem(
                 "ERRO: o Adotante ja esta cadastrado no Sistema.")
--- controladores/controlador_tipos_habitacao.py
+++ controladores/controlador_tipos_habitacao.py
@@ -12,6 +12,7 @@ class ControladorTipoHabitacao:
         self.__tela_tipo_habitacao.mostrar_mensagem(tipo_habitacao)
         return tipo_habitacao
 
+<<<<<<< Updated upstream
     def cria_casa_pequena(self):
         tipo_habitacao = ['casa', 'pequena']
         return tipo_habitacao
@@ -35,13 +36,87 @@ class ControladorTipoHabitacao:
     def cria_apartamento_grande(self):
         tipo_habitacao = TipoHabitacao('apartamento', 'grande')
         return tipo_habitacao
+=======
+    # Para alterar os atributos de tipo_habitacao
+    # O identificador do tipo de habitacao pode ser o cpf relacionado, ou seja, o cpf também seria atributo de
+    # tipo_habitacao, ja que o controlador de tipos_habitacao não conhece a classe adotante
+
+    def incluir_tipo_habitacao(self):
+        dados_adotante = self.__tela_adotante.pega_dados_adotante()
+        
+        cpf_valido = self.pega_adotante_por_cpf(dados_adotante['cpf'])
+        if cpf_valido is None:
+            tem_outros_animais = dados_adotante["tem_outros_animais"].upper()
+            if tem_outros_animais == "S" or tem_outros_animais == "N":
+                if tem_outros_animais == "S":
+                    tem_outros_animais = True
+                else:
+                    tem_outros_animais = False
+                adotante = Adotante(
+                    dados_adotante["cpf"], dados_adotante["nome"], dados_adotante["nascimento"], dados_adotante["endereco"], tem_outros_animais)
+                self.__adotantes.append(adotante)
+                self.__tela_adotante.mostra_mensagem(
+                    "Adotante cadastrado com sucesso no sistema")
+            else:
+                self.__tela_adotante.mostra_mensagem(
+                    "ERRO: informações inválidas, tente novamente")
+                self.__tela_adotante.pega_dados_adotante()
+        else:
+            self.__tela_adotante.mostra_mensagem(
+                "ERRO: o Adotante ja esta cadastrado no Sistema.")
+
+    def listar_adotantes(self):
+        tam_lista_adotantes = len(self.__adotantes)
+        if tam_lista_adotantes > 0:
+            for adotante in self.__adotantes:
+                self.__tela_adotante.mostra_adotante(
+                    {"cpf": adotante.cpf, "nome": adotante.nome, "nascimento": adotante.nascimento, "endereco": adotante.endereco, "tem_outros_animais": adotante.tem_outros_animais})
+        else:
+            self.__tela_adotante.mostra_mensagem(
+                "ERRO: Não existe nenhum adotante cadastrado no Sistema.")
+
+    def alterar_adotante(self):
+        self.listar_adotantes()
+        cpf_adotante = self.__tela_adotante.seleciona_adotante()
+        adotante = self.pega_adotante_por_cpf(cpf_adotante)
+
+        if (adotante is not None):
+            novos_dados_adotante = self.__tela_adotante.pega_dados_adotante()
+            adotante.cpf = novos_dados_adotante["cpf"]
+            adotante.nome = novos_dados_adotante["nome"]
+            adotante.nascimento = novos_dados_adotante["nascimento"]
+            adotante.endereco = novos_dados_adotante["endereco"]
+            adotante.tem_outros_animais = novos_dados_adotante["tem_outros_animais"]
+            self.listar_adotantes()
+        else:
+            self.__tela_adotante.mostra_mensagem(
+                "ERRO: O Adotante não existe.")
+
+    def excluir_adotante(self):
+        self.listar_adotantes()
+        cpf_adotante = self.__tela_adotante.seleciona_adotante()
+        adotante = self.pega_adotante_por_cpf(cpf_adotante)
+
+        if (adotante is not None):
+            self.__adotantes.remove(adotante)
+            self.__tela_adotante.mostra_mensagem(
+                f"Adotante de cpf: {cpf_adotante} foi excluido do sistema.")
+            self.listar_adotantes()
+        else:
+            self.__tela_adotante.mostra_mensagem(
+                "ERRO: O Adotante não existe.")
+>>>>>>> Stashed changes
 
     def retornar(self):
         self.__controlador_sistema.abre_tela()
 
     def abre_tela(self):
+<<<<<<< Updated upstream
         lista_opcoes = {1: self.cria_casa_pequena, 2: self.cria_casa_media, 3: self.cria_casa_grande, 4: self.cria_apartamento_pequeno,
                         5: self.cria_apartamento_medio, 6: self.cria_apartamento_grande, 0: self.retornar}
+=======
+        lista_opcoes = {0: self.retornar}
+>>>>>>> Stashed changes
 
         continua = True
         while continua:
